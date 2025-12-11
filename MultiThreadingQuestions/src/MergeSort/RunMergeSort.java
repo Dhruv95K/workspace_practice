075bbc58e5b29ba27ee6e451bd45316381d528ff
Dhruv Khandelwal <dhruv.khandelwal@circles.asia>
2025-12-11 13:54:08 +0530

    multithreading based questions - Implement Parallel MergeSort

---
+++ MultiThreadingQuestions/src/MergeSort/RunMergeSort.java
@@ -0,0 +1,42 @@
+package MergeSort;
+
+import java.util.Random;
+
+public class RunMergeSort {
+    public static void main(String[] args) {
+        // Create an array with 1000 random integers
+        int[] arrayToSort = new int[1000];
+        Random random = new Random();
+        for (int i = 0; i < arrayToSort.length; i++) {
+            arrayToSort[i] = random.nextInt(10000); // Random numbers between 0 and 9999
+        }
+
+        System.out.println("Array size: " + arrayToSort.length);
+        System.out.println("Testing single-threaded merge sort...");
+        SingleThreadedMergeSort singleThreadedMergeSort = new SingleThreadedMergeSort(arrayToSort);
+        int[] sortedArray = singleThreadedMergeSort.returnSortedArray();
+        singleThreadedMergeSort.getTimeTaken();
+        System.out.print("Sorted array via single threaded mergesort is: ");
+        for (int num : sortedArray) {
+            System.out.print(num + " ");
+        }
+
+        System.out.println("\nTesting multi-threaded merge sort...");
+        MultiThreadedMergeSort multiThreadedMergeSort = new MultiThreadedMergeSort(arrayToSort);
+        int[] sortedArrayMT = multiThreadedMergeSort.returnSortedArray();
+        multiThreadedMergeSort.getTimeTaken();
+        System.out.print("Sorted array via multi threaded mergesort is: ");
+        for (int num : sortedArrayMT) {
+            System.out.print(num + " ");
+        }
+    }
+
+    private static boolean isSorted(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < array[i - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ MultiThreadingQuestions/src/MergeSort/SingleThreadedMergeSort.java
@@ -0,0 +1,67 @@
+package MergeSort;
+
+public class SingleThreadedMergeSort {
+    private final int[] array;
+    private final int[] tempArray;
+    private long timeForMergeSort;
+    public SingleThreadedMergeSort(int[] arrayToSort) {
+        this.array = arrayToSort;
+        this.tempArray = new int[arrayToSort.length];
+        timeForMergeSort = 0;
+    }
+    public int[] returnSortedArray() {
+        long startTime = System.currentTimeMillis();
+        mergeSort(this.array, 0, this.array.length - 1);
+        long endTime = System.currentTimeMillis();
+        timeForMergeSort = endTime - startTime;
+        return this.array;
+    }
+    private void mergeSort(int[] array, int start, int end) {
+        if(start >= end) {
+            return;
+        }
+
+        int mid = start + (end - start) / 2;
+        mergeSort(array, start, mid);
+        mergeSort(array, mid + 1, end);
+
+        int i = start, j = mid + 1, k = start;
+        while (i <= mid && j <= end) {
+            if(array[i] <= array[j]) {
+                tempArray[k] = array[i];
+                i++;
+            }
+            else if(array[j] < array[i]){
+                tempArray[k] = array[j];
+                j++;
+            }
+            k++;
+        }
+        while(i <= mid) {
+            tempArray[k] = array[i];
+            k++;i++;
+        }
+        while (j <= end) {
+            tempArray[k] = array[j];
+            k++;j++;
+        }
+
+        for(int index = start; index <= end; index++) {
+            array[index] = tempArray[index];
+        }
+
+//        System.out.print("current state is: ");
+//        printArray();
+    }
+
+    public void printArray() {
+        int n = array.length;
+        for (int j : array)
+            System.out.print(j + " ");
+
+        System.out.println();
+    }
+    public void getTimeTaken(){
+        System.out.println("Time taken for single threaded merge sort: " + timeForMergeSort + " ms");
+    }
+}
